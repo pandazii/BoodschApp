e8995898405e7dcbe731e384bc18486a22fa115d
Tony <tonytje9@hotmail.com>
2019-09-10 21:33:51 +0200

    Commit

--- js/controllers.js
+++ js/controllers.js
@@ -1,12 +1,20 @@
 angular.module('boodschapp')
     .controller('homeController', function ($scope, $http) {
 
+
+        $scope.arr = [];
+        $scope.list = [];
+
         function getProducts() {
             $http.get('models/producten.json').then(function (data) {
-                $scope.producten = data;
+                $scope.producten = data.data;
                 console.log($scope.producten);
             });
         }
 
         getProducts();
+
+        $scope.addProductsToList = function (index) {
+            $scope.list.push(index);
+        }
     })

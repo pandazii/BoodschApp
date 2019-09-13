8857ac24af45be2567f33315d3d3ebbf3280fdd5
Tony <tonytje9@hotmail.com>
2019-09-14 00:16:33 +0200

    commit

--- js/controllers.js
+++ js/controllers.js
@@ -1,42 +1,61 @@
 angular.module('boodschapp')
-    .controller('homeController', function ($scope, $http) {
+    .controller('homeController', function ($scope, $http, $filter) {
 
-
-        $scope.arr = [];
-        $scope.list = [];
+        $scope.shoppingList = [];
         $scope.strikedList = [];
+        var shoppingListOrderd = false;
+
+        getProducts();
 
         function getProducts() {
             $http.get('models/producten.json').then(function (data) {
                 $scope.producten = data.data;
-               
-            });
+
+            }).catch(function (error) {
+                console.log("fout bij het ophalen van de producten" + error)
+            })
         }
 
-        getProducts();
+        // sorteer producten in de boodschappenlijst op product naam
+        $scope.sortShoppingListProductName = function () {
+            if(shoppingListOrderd){
+                $scope.shoppingList = $filter('orderBy')($scope.shoppingList, '-product');
+                $scope.shoppingList = $filter('orderBy')($scope.shoppingList, 'done');
+                shoppingListOrderd = false;
+            }else{
+                $scope.shoppingList = $filter('orderBy')($scope.shoppingList, 'product');
+                $scope.shoppingList = $filter('orderBy')($scope.shoppingList, 'done');
+                shoppingListOrderd = true;
+            }  
+        }
 
-        $scope.addProductsToList = function (index) {
-            
-            console.log($scope.list)
+        // Plaats afgevinkte producten in de boodschappenlijst onderaan.
+        $scope.checkboxChanged = function(){
+            $scope.shoppingList = $filter('orderBy')($scope.shoppingList, 'done');
+        }
 
-            var inList = false;
+        $scope.addProductToShoppingList = function (index, aantalProducten) {
 
-            for(var i = 0; i < $scope.list.length; i++){
-                if($scope.list[i].product == index.product){
-                    var inList = true;
+            let inShoppingList = false;
+
+            // Controle of het product al bestaat in de boodschappenlijst.
+            for (var i = 0; i < $scope.shoppingList.length; i++) {
+                if ($scope.shoppingList[i].product == index.product) {
+                    inShoppingList = true;
                 }
-            } 
-            
-            if(!inList){
-                $scope.list.push(index);
             }
 
+            // Wanneer het product niet in de lijst voorkomt, voeg dan toe aan de lijst. Indien het product voorkomt in de lijst, pas dan alleen het aantal van het product aan.
+            if (!inShoppingList) {
+                index.aantal = aantalProducten;
+                $scope.shoppingList.push(index);
+            } else {
+                for (var i = 0; i < $scope.shoppingList.length; i++) {
+                    if ($scope.shoppingList[i].product == index.product) {
+                        $scope.shoppingList[i].aantal += aantalProducten
+
+                    }
+                }
+            }
         }
-
-        $scope.deleteProductFromList = function (index) {
-            $scope.list.splice(index);
-        }
-
-       console.log()
-
     })
--- js/routing.js
+++ js/routing.js
@@ -2,7 +2,7 @@ angular.module('boodschapp')
     .config(function ($routeProvider) {
         $routeProvider
             .when("/", {
-                templateUrl: "views/home.html",
+                templateUrl: "views/home_v2.html",
                 controller: 'homeController'
             })
             .when("/producten", {
